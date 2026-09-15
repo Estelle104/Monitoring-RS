e9e7b944eba091401ffed3d6b66d4fb182e17d4b
rohysafe <rohysafe@gmail.com>
2026-09-15 10:20:58 +0300

    Fix scan , quota

--- Monitoring/backend/app/main.py
+++ Monitoring/backend/app/main.py
@@ -20,7 +20,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(firewall.router, tags=["Firewall"])
+# Inclure les routes du firewall sous le préfixe /api
+app.include_router(firewall.router, prefix="/api", tags=["Firewall"])
 
 # Inclure les routes VLAN / Salle / Port
 app.include_router(salle.router, prefix="/api", tags=["Salle"])
